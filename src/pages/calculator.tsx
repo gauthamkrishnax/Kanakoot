2073252a8039f21cc4aa2b9dad631738b936115d
Gautham Krishna <gauthamkrishnax@gmail.com>
2021-07-29 12:39:53 +0530

    Front end init

---
+++ src/pages/calculator.tsx
@@ -0,0 +1,11 @@
+export interface CalculatorPageProps {}
+
+const CalculatorPage: CalculatorPageProps = () => {
+  return (
+    <div>
+      <h1>Calculator</h1>
+    </div>
+  );
+};
+
+export default CalculatorPage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import * as React from "react";
 // markup
 const IndexPage = () => {
-  return <div>Index</div>;
+  return (
+    <div>
+      <h1>Coming Soon</h1>
+    </div>
+  );
 };
 
 export default IndexPage;
